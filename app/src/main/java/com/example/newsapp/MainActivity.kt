f75a1b3e3e2eeb9d2b0f33898cfeeddb1826f568
SyauqiAlifI <vstudio776@gmail.com>
2021-12-05 21:16:46 +0700

    Errors update 1

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.newsapp
 
-import com.example.newsapp.adapter.SectionPagerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.newsapp.databinding.ActivityMainBinding
