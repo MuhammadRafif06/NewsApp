1c0f546e946a6bd6d9c1c988119736dbcc5c8758
SyauqiAlifI <vstudio776@gmail.com>
2021-12-05 20:58:51 +0700

    popularFragment update

--- app/src/main/java/com/example/newsapp/fragment/PopularFragment.kt
+++ app/src/main/java/com/example/newsapp/fragment/PopularFragment.kt
@@ -5,17 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
+import com.example.newsapp.adapter.NewsAdapter
+import com.example.newsapp.bindingNewsHeadline
+import com.example.newsapp.data.DataNews
+import com.example.newsapp.databinding.FragmentPopularBinding
 
 
 class PopularFragment : Fragment() {
+
+    private lateinit var binding: FragmentPopularBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this com.example.newsapp.fragment
-        return inflater.inflate(R.layout.fragment_popular, container, false)
+
+        binding = FragmentPopularBinding.inflate(inflater, container, false)
+
+        binding.rvPopularNews.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = NewsAdapter(DataNews.dataPopularNews)
+        }
+
+        bindingNewsHeadline(binding.newsHeadline, 1)
+
+        return binding.root
     }
 }
 
